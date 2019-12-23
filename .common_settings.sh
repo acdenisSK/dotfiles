1c860d99f5867fc4c41cb702eea6e9d634184809
Alex M. M <acdenissk69@gmail.com>
2019-12-23 19:45:26 +0100

    Being a bit dynamic shouldn't hurt.

--- .common_settings.sh
+++ .common_settings.sh
@@ -3,11 +3,20 @@
 export LANG=en_US.UTF-8
 
 alias ls="ls --color=auto"
-alias pacman="pacman --color=auto"
-alias yay="yay --color=auto"
-alias cargo="cargo --color=auto"
+
+# Only apply these if the programs already exist
+[ -x /usr/bin/pacman ] && alias pacman="pacman --color=auto"
+[ -x /usr/bin/yay ] && alias yay="yay --color=auto"
+[ -x /usr/bin/cargo ] && alias cargo="cargo --color=auto"
+
+if [ -x /usr/bin/git ] && [ -d $HOME/dotfiles ]; then
+    alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+fi
 
 export VISUAL="nvim"
 export EDITOR="${VISUAL}"
 
-alias config="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+NVM_PATH=/usr/share/nvim/init-nvm.sh
+
+[ -f $NVM_PATH ] && source $NVM_PATH
+
